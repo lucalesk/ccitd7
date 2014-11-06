98349e182f1c3dc2d58e2e4f23a7f63988a4726f
emlucalesk/em <emlucalesk@users.noreply.github.com/em>
2014-11-06 17:04:03 +0100

    First commit of all files

---
+++ page.tpl.php
@@ -0,0 +1,215 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
+?>
+
+  <div id="container">
+
+    <div class="topbar">
+		<div class="topbar-inner">
+			<div class="container">
+			
+			
+
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <?php if ($title): ?>
+              <div id="site-name"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </strong></div>
+            <?php else: /* Use h1 when the content title is empty */ ?>
+              <h1 id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+        </div> <!-- /#name-and-slogan -->
+      <?php endif; ?>
+
+      <?php print render($page['header']); ?>
+
+    </div></div></div> <!-- /#topbar, /#topbar-inner, /#container -->
+	
+
+	
+
+    <?php if ($main_menu || $secondary_menu): ?>
+      <div id="navigation"><div class="section">
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+      </div></div> <!-- /.section, /#navigation -->
+    <?php endif; ?>
+
+    <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+	<?php if (user_is_logged_in() && $show_messages && $messages): print $messages; endif; ?>
+	
+	<div id="main" role="main">
+		<div class="container">
+			<div class="sixteen columns">
+				<div class="first row">
+					
+					
+						
+					<?php print render($page['content']); ?>
+				
+											
+
+				</div>
+				
+				<?php if($is_front) :?>
+				
+					<div class="short row">
+					<?php if ($page['short_row_left']): ?>
+					<div class="five columns alpha"><div class="bucket"><div class="inner">
+						<?php print render($page['short_row_left']); ?>
+					</div></div></div>
+					<?php endif; ?>
+					
+					<?php if ($page['short_row_center']): ?>
+					<div class="six columns"><div class="bucket"><div class="inner">
+						<?php print render($page['short_row_center']); ?>
+					</div></div></div>
+					<?php endif; ?>
+					
+					<?php if ($page['short_row_right']): ?>
+					<div class="five columns omega"><div class="bucket"><div class="inner">
+						<?php print render($page['short_row_right']); ?>
+					</div></div></div>
+					<?php endif; ?>
+					
+					</div>
+							
+					<div class="tall row">
+					<?php if ($page['tall_row_left']): ?>
+					<div class="five columns alpha"><div class="bucket"><div class="inner">
+						<?php print render($page['tall_row_left']); ?>
+					</div></div></div>
+					<?php endif; ?>
+
+					<?php if ($page['tall_row_center']): ?>
+					<div class="six columns"><div class="bucket"><div class="inner">
+						<?php print render($page['tall_row_center']); ?>
+					</div></div></div>
+					<?php endif; ?>
+
+					<?php if ($page['tall_row_right']): ?>
+					<div class="five columns omega"><div class="bucket"><div class="inner">
+						<?php print render($page['tall_row_right']); ?>
+					</div></div></div>
+					<?php endif; ?>
+					</div>
+				
+				<?php endif ?>
+				
+				
+				<?php if ($page['administrative']): ?>
+				<div class="tall row">
+					<?php print render($page['administrative']); ?>
+				</div>
+				<?php endif; ?>
+				
+
+				
+			</div>
+		</div>
+	</div>
+
+ 
+
+  
+		<?php if ($page['footer']): ?>
+		<footer id="fixfooter">
+			<div class="container">
+			<div id="globe"></div>
+				<div id="colophon"><div class="sixteen columns"><div class="first row">
+				
+					<?php print render($page['footer']); ?>
+					
+				</div></div></div>
+				
+			</div>
+		</footer>
+		<?php endif; ?>  
+  
+   </div> <!-- /#main --> 
---
+++ views-view-fields--featured-slider-2.tpl.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($fields as $id => $field): ?>
+  <?php if (!empty($field->separator)): ?>
+    <?php print $field->separator; ?>
+  <?php endif; ?>
+
+  <?php print $field->wrapper_prefix; ?>
+    <?php print $field->label_html; ?>
+	
+		<?php if($field->class=='title'): ?>
+		<div class="five columns alpha"><div class="content">
+			<h6><?php print $field->content; ?></h6>
+		<?php endif ?>
+
+			<?php if($field->class=='body'): ?>
+			<p><?php print $field->content; ?></p>
+			<div class="bucket-follow">
+				<a class="primary btn" href="./news">Tutte le notizie</a>
+</div>
+		</div></div>
+		<?php endif ?>
+	
+	    <?php if($field->class=='field-slider-image'): ?>
+		<div class="eleven columns omega"><div class="film">
+			<?php print $field->content; ?>
+		</div></div>
+	<?php endif ?>
+	
+	
+  <?php print $field->wrapper_suffix; ?>
+<?php endforeach; ?>
